0b9b1bf92045f5aeddb3462bfee15b4422d0fadf
Gregory <lobster287@gmail.com>
2022-05-16 00:13:35 +0700

    Refactored tests

--- CustomPackages/DudesComparer.Tests/Tests.cs
+++ CustomPackages/DudesComparer.Tests/Tests.cs
@@ -1,5 +1,8 @@
 namespace DudesComparer.Tests
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using DudesComparer.Models;
@@ -11,53 +14,83 @@ namespace DudesComparer.Tests
 
     public class Tests
     {
-        [Test]
-        public async Task CompareTwoDudesTestAsync()
+        [TestCaseSource(typeof(CompareDudesCases))]
+        public async Task CompareDudesTestAsync(string testCaseName, ComparingDudes inputData, ComparedDudes expected)
         {
             var dudesHandler = DudesHandlerFactory.GetInitializedDudesHandler();
-            var dudesToCompare = new ComparingDudes
-            {
-                ChatId = new ChatId("testChatId"),
-                DudesUserNames = new[] {"dude1", "dude2"}
-            };
-            var actualComparedDudes = await dudesHandler.CompareDudesAsync(dudesToCompare);
-            var expectedComparedDudes = new ComparedDudes
-            {
-                DudeInfos = new DudeInfo[]
-                {
-                    new()
-                    {
-                        DudeType = DudeTypes.Winner,
-                        CockSize = new CockSize(10),
-                        UserInfo = new UserInfo
-                        {
-                            UserId = 1,
-                            FirstName = "dude1FN",
-                            LastName = "dude1LN",
-                            Username = "dude1"
-                        }
-                    },
-                    new()
-                    {
-                        DudeType = DudeTypes.Loser,
-                        CockSize = new CockSize(5),
-                        UserInfo = new UserInfo
-                        {
-                            UserId = 2,
-                            FirstName = "dude2FN",
-                            LastName = "dude2LN",
-                            Username = "dude2"
-                        }
-                    }
-                }
-            };
+            var actualComparedDudes = await dudesHandler.CompareDudesAsync(inputData);
 
-            expectedComparedDudes.Should().BeEquivalentTo(actualComparedDudes);
+            expected.Should().BeEquivalentTo(actualComparedDudes);
         }
 
         [SetUp]
         public void Setup()
         {
+        }
+    }
+
+    internal record CompareDudeTestCase
+    {
+        public string TestCaseName { get; init; } = null!;
+        public ComparedDudes Expected { get; init; } = null!;
+
+        public ComparingDudes InputData { get; init; } = null!;
+    }
+
+    internal class CompareDudesCases : IEnumerable
+    {
+        private static readonly IReadOnlyCollection<CompareDudeTestCase> TestCases = new[]
+        {
+            GetCompareTwoDudesCompareTestCase()
+        };
+
+        public IEnumerator GetEnumerator()
+        {
+            return TestCases.Select(x => new object[] {x.TestCaseName, x.InputData, x.Expected})
+                            .GetEnumerator();
+        }
+
+        private static CompareDudeTestCase GetCompareTwoDudesCompareTestCase()
+        {
+            return new CompareDudeTestCase
+            {
+                TestCaseName = nameof(GetCompareTwoDudesCompareTestCase),
+                InputData = new ComparingDudes
+                {
+                    ChatId = new ChatId("testChatId"),
+                    DudesUserNames = new[] {"dude1", "dude2"}
+                },
+                Expected = new ComparedDudes
+                {
+                    DudeInfos = new DudeInfo[]
+                    {
+                        new()
+                        {
+                            DudeType = DudeTypes.Winner,
+                            CockSize = new CockSize(10),
+                            UserInfo = new UserInfo
+                            {
+                                UserId = 1,
+                                FirstName = "dude1FN",
+                                LastName = "dude1LN",
+                                Username = "dude1"
+                            }
+                        },
+                        new()
+                        {
+                            DudeType = DudeTypes.Loser,
+                            CockSize = new CockSize(5),
+                            UserInfo = new UserInfo
+                            {
+                                UserId = 2,
+                                FirstName = "dude2FN",
+                                LastName = "dude2LN",
+                                Username = "dude2"
+                            }
+                        }
+                    }
+                }
+            };
         }
     }
 }
